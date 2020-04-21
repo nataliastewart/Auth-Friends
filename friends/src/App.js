3104b52b871722ddbe6ccc4cc28405bad0cbe18e
nataliastewart <nataliastewart@users.noreply.github.com>
2020-04-21 18:42:50 -0400

    Finished to create PrivateRoute file - still not working - not displaying the data and forbidden access to protected page

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Login from "./components/Login";
 import FriendsList from "./components/FriendsList";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
           </li>
         </ul>
         <Switch>
-          <Route exact path="/protected" component={FriendsList} />
+          <PrivateRoute exact path="/protected" component={FriendsList} />
           <Route path="/login" component={Login} />
           <Route component={Login} />
         </Switch>
